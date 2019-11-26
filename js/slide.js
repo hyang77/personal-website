9c00d155dd2205322ade4882c8164a3303f27f28
Haoli Yang <hyang77@hawk.iit.edu>
2019-11-25 20:36:30 -0600

    delete photo

--- js/slide.js
+++ js/slide.js
@@ -6,11 +6,7 @@ images[0]= 'images/travel1.png';
 images[1]= 'images/travel2.jpg';
 images[2]= 'images/travel3.jpg';
 images[3]= 'images/travel4.jpg';
-<<<<<<< HEAD
 
-=======
-images[4]= 'images/travel5.jpg';
->>>>>>> 4ab6d327f83217c0c81aaffc6a54073b5a09b0a1
 //change Image
 function changeImg(){
 	document.slide.src = images[i];
