6fdcab1f888947b70e348180f6f84aa66692626d
Haoli Yang <hyang77@hawk.iit.edu>
2019-11-25 18:04:55 -0600

    create slide.js

---
+++ js/slide.js
@@ -0,0 +1,25 @@
+var i = 0; //start point
+var images = [];
+var time = 1000;
+//image lists
+images[0]= 'images/travel1.png';
+images[1]= 'images/travel2.jpg';
+images[2]= 'images/travel3.jpg';
+images[3]= 'images/travel4.jpg';
+images[4]= 'images/travel5.jpg';
+
+//change Image
+function changeImg(){
+	document.slide.src = images[i];
+	
+	if(i < images.length - 1){
+		i++;
+	} 
+	else {
+		i = 0;
+	}
+	
+	setTimeout("changeImg()",time);
+}
+
+window.onload = changeImg;
